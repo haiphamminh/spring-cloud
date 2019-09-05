eee464418ec2f0630970d5c34647276f9cd9ebce
hai_pham_minh <hai.phamminh86@gmail.com>
2019-09-05 13:24:47 +0700

    add config and discovery server

---
+++ config-server/src/test/java/com/spring/cloud/config/ConfigApplicationTest.java
@@ -0,0 +1,11 @@
+package com.spring.cloud.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
